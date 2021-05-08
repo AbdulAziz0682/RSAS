00d13500313abc8d4a868df5e5f0844a4c36ab58
AbdulAziz0682 <abdulrasheed2132@gmail.com>
2021-05-08 07:58:44 +0500

    Added Sport.java, sports.txt, students.txt, Student.java, and modified SportCenter.java.

--- src/sportAcademy/Sport.java
+++ src/sportAcademy/Sport.java
@@ -22,35 +22,35 @@ import java.util.logging.Logger;
  *
  * @author Abdul Aziz
  */
-public class SportCenter {
-    private int code;
+public class Sport {
     private String name;
-    private String location;
-    
-    public SportCenter(){}
-
-    public SportCenter(int code, String name, String location) {
+    private int code;
+    private int fees;
+    private String schedule;
+    public Sport() {}
+    public Sport(int code, String name, int fees, String schedule){
         this.code = code;
         this.name = name;
-        this.location = location;
+        this.fees = fees;
+        this.schedule = schedule;
     }
-    public static String toJson(SportCenter center){
+    public static String toJson(Sport sport){
         Gson gson = new Gson();
-        return gson.toJson(center, SportCenter.class);
+        return gson.toJson(sport, Sport.class);
     }
-    public static SportCenter fromJson(String json){
+    public static Sport fromJson(String json){
         Gson gson = new Gson(); 
-        return gson.fromJson(json, SportCenter.class);
+        return gson.fromJson(json, Sport.class);
     }
-    public static ArrayList<SportCenter> getAllSportCenteres(){
-        ArrayList<SportCenter> list = new ArrayList<SportCenter>();
-        String fileName = "src/Records/centers.txt";
+    public static ArrayList<Sport> getAllSports(){
+        ArrayList<Sport> list = new ArrayList<Sport>();
+        String fileName = "src/Records/sports.txt";
         Path path = new File(fileName).toPath();
         Gson gson = new Gson();
         try (Reader reader = Files.newBufferedReader(path, 
                 StandardCharsets.UTF_8)) {
             
-            SportCenter c[] = gson.fromJson(reader, SportCenter[].class);
+            Sport c[] = gson.fromJson(reader, Sport[].class);
             if(c!=null)
             for(int i=0; i<c.length; i++){
                 System.out.println("Record from file: "+c[i].code);
@@ -58,19 +58,19 @@ public class SportCenter {
             }
             return list;
         } catch (IOException ex) {
-            Logger.getLogger(SportCenter.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Error at SportCenter.java in getAllSportCenteres()");
+            Logger.getLogger(Sport.class.getName()).log(Level.SEVERE, null, ex);
+            System.out.println("Error at Sport.java in getAllSports()");
         }
         return list;        
     }
     public boolean saveToDisk(){
-        if(SportCenter.findInDisk(this.code)!=null) return false;
-        ArrayList<SportCenter> list = SportCenter.getAllSportCenteres();
+        if(Sport.findInDisk(this.code)!=null) return false;
+        ArrayList<Sport> list = Sport.getAllSports();
         list.add(this);
         return this.saveAllToDisk(list);
     }
-    private static boolean saveAllToDisk(ArrayList<SportCenter> list){
-        String fileName = "src/Records/centers.txt";        
+    private static boolean saveAllToDisk(ArrayList<Sport> list){
+        String fileName = "src/Records/sports.txt";        
         try (FileOutputStream fos = new FileOutputStream(fileName);
                 OutputStreamWriter isr = new OutputStreamWriter(fos, 
                         StandardCharsets.UTF_8)) {
@@ -80,12 +80,12 @@ public class SportCenter {
             return true;
         } catch (IOException ex) {
             Logger.getLogger(Coach.class.getName()).log(Level.SEVERE, null, ex);
-            System.out.println("Error at SportCenter.java in saveAllToDisk()");
+            System.out.println("Error at Sport.java in saveAllToDisk()");
         }
         return false;
     }
-    public static SportCenter findInDisk(int code){
-        ArrayList<SportCenter> list = SportCenter.getAllSportCenteres();
+    public static Sport findInDisk(int code){
+        ArrayList<Sport> list = Sport.getAllSports();
         for(int i=0; i<list.size(); i++){
             if(code==list.get(i).code){
                 return list.get(i); 
@@ -94,33 +94,14 @@ public class SportCenter {
         return null;
     }
     public static boolean deleteFromDisk(int code){
-        ArrayList<SportCenter> list = SportCenter.getAllSportCenteres();
+        ArrayList<Sport> list = Sport.getAllSports();
         for(int i=0; i<list.size(); i++){
             if(code==list.get(i).code){
                 list.remove(i);
-                return SportCenter.saveAllToDisk(list); 
+                return Sport.saveAllToDisk(list); 
             }
         }
         return false;
     }
-    
-    public int getCode() {
-        return code;
-    }
-    public void setCode(int code) {
-        this.code = code;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getLocation() {
-        return location;
-    }
-    public void setLocation(String location) {
-        this.location = location;
-    }
-    
+
 }
---
+++ src/sportAcademy/Student.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sportAcademy;
+
+/**
+ *
+ * @author Abdul Aziz
+ */
+public class Student {
+    private String name;
+    private int id = (int) (Math.random()*Math.random()*10000);
+    
+}
